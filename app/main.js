065be521cb62ac7593ae690374a85405078fb78c
Elliot Lewis <lewell94@gmail.com>
2016-10-13 14:08:33 +0100

    angular 2 Hello World

---
+++ app/main.js
@@ -0,0 +1,6 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_module_1 = require('./app.module');
+var PLATFORM = platform_browser_dynamic_1.platformBrowserDynamic();
+PLATFORM.bootstrapModule(app_module_1.AppModule);
+//# sourceMappingURL=main.js.map
